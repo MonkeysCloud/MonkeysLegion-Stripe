901268f38d7cdba9e97565321cc8cfabc7e074d4
Amanar Marouane <marouane.amanar07@gmail.com>
2025-06-14 21:51:01 +0100

    Change: func name

--- src/Config/merge.php
+++ src/Config/merge.php
@@ -5,11 +5,11 @@
  * @param array $vendor The vendor configuration array.
  * @param array $app The app configuration array.
  */
-function mergeStripeConfig(array $vendor, array $app): array
+function configMerger(array $vendor, array $app): array
 {
     foreach ($app as $k => $v) {
         if (is_array($v) && isset($vendor[$k])) {
-            $vendor[$k] = mergeStripeConfig($vendor[$k], $v);
+            $vendor[$k] = configMerger($vendor[$k], $v);
         } else {
             $vendor[$k] = $v;
         }
